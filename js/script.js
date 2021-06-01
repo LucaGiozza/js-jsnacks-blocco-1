e2b791c3da9cba4d7844a44ba13533768f42ab1d
Luca Giozza <luca.giozza@gmail.com>
2021-06-01 15:33:32 +0200

    secondo esercizio con for:

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 //  1 =L’utente inserisce due parole in successione, con due prompt.
 //  Il software stampa prima la parola più corta,
 //   poi la parola più lunga.
-//  2=Il software deve chiedere per 5 volte all’utente di inserire
-//  un numero. Il programma stampa la somma di tutti i numeri 
-//    inseriti. Esegui questo programma in due versioni, con il
-//    for e con il while.
+// 
 //  3=Crea un array vuoto. Chiedi per 6 volte all’utente di 
 // inserire un numero, se è dispari inseriscilo nell’array
 //  4=In un array sono contenuti i nomi degli invitati alla festa
@@ -14,18 +11,43 @@
 // primo esercizio :
 
 
- var parola = prompt();
- var secondaParola = prompt();
+//  var parola = prompt();
+//  var secondaParola = prompt();
 
- var first = parola.length;
- var second = secondaParola.length;
+//  var first = parola.length;
+//  var second = secondaParola.length;
 
- if(first === second){
-     document.getElementById("scritta").innerHTML = 'Sono ugiuali' + ' ' + parola + ' ' + secondaParola; 
+//  if(first === second){
+//      document.getElementById("scritta").innerHTML = 'Sono ugiuali' + ' ' + parola + ' ' + secondaParola; 
     
-     }else if(first < second) {
-         document.getElementById("scritta").innerHTML = parola + ' ' + secondaParola  ;
-     }else{
-         document.getElementById('scritta').innerHTML = secondaParola + ' ' + parola;
-     }
+//      }else if(first < second) {
+//          document.getElementById("scritta").innerHTML = parola + ' ' + secondaParola  ;
+//      }else{
+//          document.getElementById('scritta').innerHTML = secondaParola + ' ' + parola;
+//      }
+
+// secondo esercizio:
+ //2=Il software deve chiedere per 5 volte all’utente di inserire
+ // un numero. Il programma stampa la somma di tutti i numeri 
+ // inseriti. Esegui questo programma in due versioni, con il
+ // for e con il while.
+
+//  con for:
+
+   var somma = 0;
+
+   for ( var i = 0 ; i < 5; i++){
+      numero = parseInt(prompt('inserisci il numero'));
+      console.log(numero);
+      somma  = somma + numero ;
+  }
+
+   document.getElementById('calcolo').innerHTML = somma;
+   console.log(somma)
+
+
+
+
+
+
     
