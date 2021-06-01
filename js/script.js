49cc9df2da29c6d4bb6a5cbb5c8194cabd735623
Luca Giozza <luca.giozza@gmail.com>
2021-06-01 15:14:27 +0200

    primo esercizio

--- js/script.js
+++ js/script.js
@@ -10,3 +10,22 @@
 //  4=In un array sono contenuti i nomi degli invitati alla festa
 //  del grande Gatsby, chiedi all’utente il suo nome e
 //   comunicagli se può partecipare o no alla festa.
+
+// primo esercizio :
+
+
+ var parola = prompt();
+ var secondaParola = prompt();
+
+ var first = parola.length;
+ var second = secondaParola.length;
+
+ if(first === second){
+     document.getElementById("scritta").innerHTML = 'Sono ugiuali' + ' ' + parola + ' ' + secondaParola; 
+    
+     }else if(first < second) {
+         document.getElementById("scritta").innerHTML = parola + ' ' + secondaParola  ;
+     }else{
+         document.getElementById('scritta').innerHTML = secondaParola + ' ' + parola;
+     }
+    
